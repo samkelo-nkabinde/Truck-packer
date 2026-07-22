c9826611671c3737dde2a62c9291726b9a1b7f86
Nkabinde, S, Mr [28118944@sun.ac.za] <28118944@sun.ac.za>
2026-07-22 14:54:24 +0200

    Extended greeter function [#6]

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,5 +1,16 @@
-public class Main {
+public class Greeter {
+    public static void greeter(String name) {
+        System.out.println("Hello, " + name + 
+                        ". Your name starts with the letter: " + "'" +
+                        Character.toUpperCase(name.charAt(0)) + "'" + 
+                        ", and end ends with the letter: " + "'"+
+                        Character.toLowerCase(name.charAt(name.length() - 1)) + "'" + 
+                        ", It has " + name.length() + " letters!");
+        return;
+    }
+
     public static void main(String args[]) {
+        greeter(args[0]); // Assuming the user will pass their name 
         return;
     }
 }
