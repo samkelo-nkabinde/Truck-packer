2df8d21c9fd86dbca3e3959fca66a83a73d3c27e
Nkabinde, S, Mr [28118944@sun.ac.za] <28118944@sun.ac.za>
2026-07-26 13:31:44 +0200

    I am definitely going to start a University No change of mind! [#9]

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,5 +1,15 @@
-public class Main {
+import java.util.Scanner;
+
+public class Greeter {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String getName() {
+        System.out.print("Enter your name: ");
+        return scanner.nextLine();
+    }
+
     public static void main(String args[]) {
-        return;
+        String name = getName();
+        System.out.println("Hello, " + name + "!");
     }
 }
