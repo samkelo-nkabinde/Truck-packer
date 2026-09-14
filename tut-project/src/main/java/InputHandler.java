89ce81d83ac2632f70d5fe68d277480e4ed2c8a2
Nkabinde, S, Mr [28118944@sun.ac.za] <28118944@sun.ac.za>
2026-09-14 19:36:04 +0200

    Extend input handle to allow multiple truck

--- tut-project/src/main/java/InputHandler.java
+++ tut-project/src/main/java/InputHandler.java
@@ -6,6 +6,9 @@ public class InputHandler {
     private int truckVolume;
     private int maxItems;
     private List<Item> inventory;
+    
+    // for multiple trucks 
+    private List<Truck> fleet;
 
     public InputHandler() {} 
     
@@ -13,9 +16,11 @@ public class InputHandler {
     public int getTruckVolume() { return truckVolume; }
     public int getMaxItems() { return maxItems; }
     public List<Item> getInventory() { return inventory; }
+    public List<Truck> getFleet() { return fleet; } // for multiple trucks
 
     // modifier methos
     public void setTruckVolume(int truckVolume) { this.truckVolume = truckVolume; }
     public void setMaxItems(int maxItems) { this.maxItems = maxItems; }
     public void setInventory(List<Item> inventory) { this.inventory = inventory; }
+    public void setFleet(List<Truck> fleet) { this.fleet = fleet; } 
 }
