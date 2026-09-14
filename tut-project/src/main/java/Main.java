db11d8841d11c2b314ff80bd4820584f6372ce44
Nkabinde, S, Mr [28118944@sun.ac.za] <28118944@sun.ac.za>
2026-09-14 21:37:36 +0200

    Update main to support XML

--- tut-project/src/main/java/Main.java
+++ tut-project/src/main/java/Main.java
@@ -1,6 +1,7 @@
 package truckpacker;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper; 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -8,24 +9,41 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] argv){
+        
+        if (argv.length > 0 && argv[0].equals("--server")) {
+            try {
+                TruckServer.startServer();
+            } catch (IOException e) {
+                System.err.println("Error: Unable to start the server.");
+            }
+            return;
+        }
 
-        if (argv.length < 1) {
-            System.err.println("Error: Please provide an input file path.");
+        if (argv.length < 2 || !argv[0].equals("--file")) {
+            System.err.println("Error: Please provide '--server' or '--file <input path>'.");
             return;
         }
         
-        String inputFilePath = argv[0];
-        String outputFilePath = inputFilePath.replace(".json", "_output.json");
+        String inputFilePath = argv[1]; // The path is now the second argument
+        
+        // XML output extension
+        String outputFilePath = inputFilePath.replace(".json", "_output.json").replace(".xml", "_output.xml");
             
-        // Just in case input file didn't have a .json extension
         if (outputFilePath.equals(inputFilePath)) {
             outputFilePath = inputFilePath + "_output.json";
         }
 
         InputHandler inputData;
-        ObjectMapper mapper = new ObjectMapper();
+        
+        // Use XmlMapper if the file is XML 
+        ObjectMapper mapper;
+        if (inputFilePath.toLowerCase().endsWith(".xml")) {
+            mapper = new XmlMapper();
+        } else {
+            mapper = new ObjectMapper();
+        }
 
-        // Parse the input JSON file
+        // Parse the input file
         try {
             inputData = mapper.readValue(new File(inputFilePath), InputHandler.class);
         } catch (IOException e) {
@@ -34,7 +52,6 @@ public class Main {
         }
 
        
-        
         if (inputData.getFleet() != null && !inputData.getFleet().isEmpty()) {
             // tut 3
             TruckPacker packer = new TruckPacker();
@@ -44,9 +61,7 @@ public class Main {
                 System.err.println("Error: Impossible to create a packing list that meets the requirements.");
                 return;
             }
-            
-        
-            
+
             // double totalCost = 0;
             // for (Truck truck : packedTrucks) {
             //     System.out.println("Truck ID: " + truck.getId() + " | Cost: R" + truck.getCost());
@@ -57,9 +72,8 @@ public class Main {
             //     }
             //     totalCost += truck.getCost();
             // }
-           
-            
-            // Write manifest to output file
+
+            // Write to output file
             try {
                 mapper.writerWithDefaultPrettyPrinter().writeValue(new File(outputFilePath), packedTrucks);
             } catch (IOException e) {
